53fbe16d56d493b18ae2e6ddce81d2c7ec074f8f
Widiz <dev@widiz.com>
2018-07-14 08:18:06 -0400

    fix

--- clearLogs.sh
+++ clearLogs.sh
@@ -1,3 +1,4 @@
 cd /var/www/newplay/logs
 grep -rol 'exif_read_data' . | tr \n 0 | xargs -n1 rm -f
+grep -rol 'guzzlehttp/guzzle/src/Handler/CurlFactory.php' . | tr \n 0 | xargs -n1 rm -f
 grep -rol ': stat failed for ' . | tr \n 0 | xargs -n1 rm -f
