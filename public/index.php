7f3dbfe429cd23a3e444cdedc057aca7c85d7936
Widiz <dev@widiz.com>
2019-09-24 02:53:48 -0400

    p

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ if ($isDebug) {
 	ini_set('display_errors', 0);
 }
 
-define('STRAYBOOTS_BUILD', 1190);
+define('STRAYBOOTS_BUILD', 1191);
 
 define('APP_PATH', realpath('..'));
 define('PUBLIC_PATH', __DIR__ . '/');
